e951d22bc9aa2d25ed4d2bb16dae511c545fb3fe
Aditya Karnik <aditya.s.karnik@gmail.com>
2019-05-12 20:09:54 +0530

    readme notes and comments added

--- problem_one.py
+++ problem_one.py
@@ -1,8 +1,10 @@
+# dictionary of animals and there emblem
 animals = {'Land' : 'Panda', 'Water' : 'Octopus', 'Ice' : 'Mammoth', 'Air' : 'Owl', 'Fire' : 'Dragon'}
-
+# empty list to add the final allies
 whoall = []
 
 while True:
+    # to accept the what the user wants to check
     input_message = input("1. Who is the ruler of Southeros? \n2. Allies of Ruler? \n3. Input Messages to kingdoms from King Shan \n4. Type 'quit'\n")
     if input_message == "1":
         if len(whoall) > 2:
@@ -10,12 +12,18 @@ while True:
         else: 
             print("None")
     elif input_message == "2":
+        # to check and print the allies of king shah
         if (len(whoall) > 0):
             print(whoall)
         else:
             print("None")
     elif input_message == "3":
+        # getting user input for the message to be sent to allies
         message = input("Enter the message: ")
         for key, value in animals.items():
+            # reading the message from the input and checking each character
+            # and matching with the dictionary
+            # all() will return the values if true,
+            # then add that current key to the final allies list
             if all([char in message.lower() for char in value.lower()]): whoall.append(key)
     else: break
--- problem_two.py
+++ problem_two.py
@@ -2,12 +2,21 @@ import random
 import os
 cwd = os.getcwd()
 
+# declaring dictionary the ruler and its emblem 
 animals = {'Land' : 'Panda', 'Water' : 'Octopus', 'Ice' : 'Mammoth', 'Air' : 'Owl', 'Fire' : 'Dragon', 'Space': 'Gorilla'}
+# list of all the 6 kingdoms
 all_kingdoms = ["Air", "Land", "Ice", "Fire", "Water", "Space"]
 messages = []
+# empty values dictionary to add the allies of the kingdom
 allegiance = {'Land': [], 'Water': [], 'Ice': [], 'Air': [], 'Fire': [], 'Space': []}
 winner = {}
 
+"""
+function to selecting a random message from message list and validating it with kingdom's emblem
+also remove checking and remove the kingdom which is already in competing list
+else adding the kindom to the allegiance of that kingdom
+also returning the number if allies each kingdom has
+"""
 def valar_morghulis(competing):
     for i in range(6):
         for kingdom in competing:
@@ -28,21 +37,26 @@ def valar_morghulis(competing):
         print("Allies for",kingdom,count)
 
 
+# reading all messages from the boc-messages.txt
 messages_file = cwd+"/boc-messages.txt"
 with open(messages_file,"r") as m:
     for message in m:
         message = message.replace(",\n","").replace("\"","").strip()
         messages.append(message)
 while True:
+    # to accept the what the user wants to check
     input_message = input("\n1. Who is the ruler of Southeros? \n2. Allies of Ruler? \n3. Enter Kingdoms competing to be the ruler: \n4. Type 'quit'\n\n")
     if input_message == "1":
         print("\nWho is the ruler of Southeros?")
+        # checking from the winner list who has the maximum support number
+        # print the winner kingdom
         if (bool(winner) and winner[max(winner, key=winner.get)]>0):
             print(max(winner, key=winner.get))
         else:
             print("None")
     elif input_message == "2":
         print("\nAllies of Ruler?")
+        # getting the winner and the which kingdom are giving the support
         if (bool(winner) and winner[max(winner, key=winner.get)]>0):
             print(next(i for i in allegiance[max(winner, key=winner.get)] if len(i) > 0))
         else:
